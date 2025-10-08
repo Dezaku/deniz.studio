6d0ae662317b43abb056e14ce8ba24152f4c4779
Dezaku <denizozankumral@gmail.com>
2025-10-08 21:41:05 +0200

    fixed?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'dezaku.github.io',
-    base: '/deniz.studio'
+    site: 'https://deniz.studio',
 });

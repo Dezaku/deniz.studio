77fdb8aa559a34cf63738b88e7b95c550c6329bb
Dezaku <denizozankumral@gmail.com>
2026-06-11 02:30:32 +0200

    form fix?

--- src/components/ReachOutForm.tsx
+++ src/components/ReachOutForm.tsx
@@ -87,7 +87,7 @@ export default function ReachOutForm() {
       const formData = new FormData(e.currentTarget)
       selectedReasons.forEach((r) => formData.append("looking_for[]", r))
       formData.set("budget", budget)
-      const res = await fetch("https://formsubmit.co/ajax/mail@deniz.studio", {
+      const res = await fetch("https://formsubmit.co/ajax/a4d2b596f5f9df6e3a8bcd5cfacae347", {
         method: "POST",
         body: formData,
       })
